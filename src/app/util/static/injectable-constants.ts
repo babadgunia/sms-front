d7fc866b25226d2041d7c15f7654cb397bcbcb40
nkvaratskhelia <nkvar12@gmail.com>
2019-05-11 10:05:43 +0300

    minor changes in injectable-constats file

--- src/app/util/static/injectable-constants.ts
+++ src/app/util/static/injectable-constants.ts
@@ -1,14 +1,14 @@
 import {environment} from '../../../environments/environment';
 
-export const API_BASE_URL: string = environment.host + 'api/';
-export const AUTH_SERVICE_URL: string = API_BASE_URL + 'auth';
-export const TEXT_SERVICE_URL: string = API_BASE_URL + 'text';
-export const USER_GROUP_SERVICE_URL: string = API_BASE_URL + 'userGroup';
-export const USER_SERVICE_URL: string = API_BASE_URL + 'user';
+const API_BASE_URL: string = environment.host + 'api/';
+export const AUTH_SERVICE_URL: string = 'AUTH_SERVICE_URL';
+export const TEXT_SERVICE_URL: string = 'TEXT_SERVICE_URL';
+export const USER_GROUP_SERVICE_URL: string = 'USER_GROUP_SERVICE_URL';
+export const USER_SERVICE_URL: string = 'USER_SERVICE_URL';
 
 export const INJECTABLE_CONSTANTS: any[] = [
-	{provide: AUTH_SERVICE_URL, useValue: AUTH_SERVICE_URL},
-	{provide: TEXT_SERVICE_URL, useValue: TEXT_SERVICE_URL},
-	{provide: USER_GROUP_SERVICE_URL, useValue: USER_GROUP_SERVICE_URL},
-	{provide: USER_SERVICE_URL, useValue: USER_SERVICE_URL}
+	{provide: AUTH_SERVICE_URL, useValue: API_BASE_URL + 'auth'},
+	{provide: TEXT_SERVICE_URL, useValue: API_BASE_URL + 'text'},
+	{provide: USER_GROUP_SERVICE_URL, useValue: API_BASE_URL + 'userGroup'},
+	{provide: USER_SERVICE_URL, useValue: API_BASE_URL + 'user'}
 ];
